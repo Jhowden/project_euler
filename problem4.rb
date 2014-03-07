c48fa07184e95db66d914a6a3526c0115c20a4e4
Jonathan Howden <jonathanhowden@gmail.com>
2014-03-07 15:02:10 -0600

    finished problem 4

---
+++ problem4.rb
@@ -0,0 +1,15 @@
+def palindrome
+  palindrome = 0
+  max = 999
+  range = (100..999)
+  until max <= 100
+  	range.each do |num|
+  		pal = max * num
+  	  palindrome = pal if pal.to_s.reverse.to_i == pal && pal > palindrome
+  	end
+  	max -= 1
+  end
+  palindrome
+end
+
+puts palindrome
