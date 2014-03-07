27b9aec4ba8308e80798918c817fca4e34355687
Jonathan Howden <jonathanhowden@gmail.com>
2014-03-07 13:51:07 -0600

    finished probem 3

---
+++ problem3.rb
@@ -0,0 +1,9 @@
+def prime_factors(num)
+	primes = []
+  return primes if num == 1
+    factors = (2..num).detect { |i| num % i == 0 }
+    primes << factors
+    primes.concat(prime_factors(num / factors))
+end
+
+puts prime_factors(600851475143).max
