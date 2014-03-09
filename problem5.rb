c88d9a75c284935d124cdf87f621d4bbcc71b44f
Jonathan Howden <jonathanhowden@gmail.com>
2014-03-09 10:40:21 -0500

    refactored problem 5

--- problem5.rb
+++ problem5.rb
@@ -1,9 +1,8 @@
 def smallest_multiple
   multiples = (2..20).to_a
   value = 20
-  multiple_found = false
 
-  while !multiple_found
+  while true
     value_found = multiples.all? { |multiple| value % multiple == 0 }
     break if value_found == true
     value += 20
